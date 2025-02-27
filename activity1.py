121dc74a6e3e0e965cf11366ec45ac4706699ae3
itschloenaeemah123 <owodoyin321@yahoo.com>
2025-02-27 18:35:25 +0100

    lesson6.py

---
+++ activity1.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age; "))
+if age >= 18:
+    if age >=65:
+        print("you are a senior citizen.")
+    else:
+        print("you are not a senior citizen")
+else:
+    print("you are a minor")
